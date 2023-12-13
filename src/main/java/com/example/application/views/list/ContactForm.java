f1bf608fcaf8b7c775b67f7bb547fd5c8c90756b
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2023-12-13 12:37:57 -0600

    Se avanza hasta la sección de validación de datos

--- src/main/java/com/example/application/views/list/ContactForm.java
+++ src/main/java/com/example/application/views/list/ContactForm.java
@@ -1,7 +1,10 @@
 package com.example.application.views.list;
 
 import com.example.application.data.Company;
+import com.example.application.data.Contact;
 import com.example.application.data.Status;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -10,6 +13,8 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.shared.Registration;
 
 import java.util.List;
 
@@ -24,8 +29,12 @@ public class ContactForm extends FormLayout {
   Button delete = new Button("Delete");
   Button close = new Button("Cancel");
 
+  BeanValidationBinder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+
   public ContactForm(List<Company> companies, List<Status> statuses) {
-    addClassName("contact-form"); 
+    addClassName("contact-form");
+
+    binder.bindInstanceFields(this);
 
     company.setItems(companies);
     company.setItemLabelGenerator(Company::getName);
@@ -48,6 +57,67 @@ public class ContactForm extends FormLayout {
     save.addClickShortcut(Key.ENTER); 
     close.addClickShortcut(Key.ESCAPE);
 
+    save.addClickListener(event -> validateAndSave());
+    delete.addClickListener(event -> fireEvent(new DeleteEvent(this, binder.getBean())));
+    close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
+    binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+
     return new HorizontalLayout(save, delete, close); 
   }
+
+  private void validateAndSave() {
+    if(binder.isValid()) {
+      fireEvent(new SaveEvent(this, binder.getBean()));
+    }
+  }
+
+  public void setContact(Contact contact) {
+    binder.setBean(contact);
+  }
+
+  // Events
+  public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+    private Contact contact;
+
+    protected ContactFormEvent(ContactForm source, Contact contact) {
+      super(source, false);
+      this.contact = contact;
+    }
+
+    public Contact getContact() {
+      return contact;
+    }
+  }
+
+  public static class SaveEvent extends ContactFormEvent {
+    SaveEvent(ContactForm source, Contact contact) {
+      super(source, contact);
+    }
+  }
+
+  public static class DeleteEvent extends ContactFormEvent {
+    DeleteEvent(ContactForm source, Contact contact) {
+      super(source, contact);
+    }
+
+  }
+
+  public static class CloseEvent extends ContactFormEvent {
+    CloseEvent(ContactForm source) {
+      super(source, null);
+    }
+  }
+
+  public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
+    return addListener(DeleteEvent.class, listener);
+  }
+
+  public Registration addSaveListener(ComponentEventListener<SaveEvent> listener) {
+    return addListener(SaveEvent.class, listener);
+  }
+  public Registration addCloseListener(ComponentEventListener<CloseEvent> listener) {
+    return addListener(CloseEvent.class, listener);
+  }
+
 }
