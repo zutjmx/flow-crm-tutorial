56e1d3527ece6a954cf45cfcd03d1447ae12fd8c
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2023-12-13 17:10:22 -0600

    Se concluye la sección Make a Vaadin Flow Application an Installable PWA

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -1,6 +1,7 @@
 package com.example.application;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,6 +15,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Theme(value = "flowcrmtutorial")
+@PWA(
+        name = "Vaadin CRM",
+        shortName = "CRM",
+        offlinePath="offline.html",
+        offlineResources = { "./images/offline-icon.png"}
+)
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
